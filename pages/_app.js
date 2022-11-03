bd80ea99893993503df10ec487ea5e07328244fa
santipu <santipujolsau@gmail.com>
2022-11-03 16:33:18 +0100

    Installer Tailwind and implemented with Header component

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
 import "../styles/globals.css"
+import Head from "next/head"
 import { MoralisProvider } from "react-moralis"
+import Header from "../components/Header"
 
 function MyApp({ Component, pageProps }) {
     return (
-        <MoralisProvider initializeOnMount={false}>
-            <Component {...pageProps} />
-        </MoralisProvider>
+        <div>
+            <Head>
+                <title>NFT Marketplace</title>
+                <meta name="description" content="NFT Marketplace" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider initializeOnMount={false}>
+                <Header />
+                <Component {...pageProps} />
+            </MoralisProvider>
+        </div>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,6 @@
-import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
-import Header from "../components/Header"
 
 export default function Home() {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>NFT Marketplace</title>
-                <meta name="description" content="NFT Marketplace" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <Header></Header>
-            Hi from index!
-        </div>
-    )
+    return <div className={styles.container}>Hi from index!</div>
 }
