f1a49fbf830823d4c53897104bed9b901595a7ba
santipu <santipujolsau@gmail.com>
2022-11-07 21:06:10 +0100

    added structure in index page to return all the NFTs in page if isWeb3Enabled

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,44 @@
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
+import { useMoralis, useMoralisQuery } from "react-moralis"
+import NFTBox from "../components/NFTBox"
 
 export default function Home() {
-    return <div className={styles.container}>Hi from index!</div>
+    const { isWeb3Enabled } = useMoralis()
+    // Moralis hook takes 2 args: tableName and a function for the query
+    const { data: listedNfts, isFetching: fetchingLIstedNfts } = useMoralisQuery(
+        "ActiveItem",
+        (query) => query.limit(10).descending("tokenId")
+    )
+
+    return (
+        <div className="container mx-auto">
+            <h1 className="py-4 px-4 font-bold text-2xl">Recently listed</h1>
+            <div className="flex flex-wrap">
+                isWeb3Enabled ? (
+                {fetchingLIstedNfts ? (
+                    <div>Loading...</div>
+                ) : (
+                    listedNfts.map((nft) => {
+                        console.log(nft.attributes)
+                        const { price, nftAddress, tokenId, marketplaceAddress, seller } =
+                            nft.attributes
+                        return (
+                            <div>
+                                <NFTBox
+                                    price={price}
+                                    nftAddress={nftAddress}
+                                    tokenId={tokenId}
+                                    marketplaceAddress={marketplaceAddress}
+                                    seller={seller}
+                                    key={`${nftAddress}${tokenId}`}
+                                />
+                            </div>
+                        )
+                    })
+                )}
+                ) : <div>Web3 Currently Not Enabled</div>
+            </div>
+        </div>
+    )
 }
