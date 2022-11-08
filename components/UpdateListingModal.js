c0e988136f45726bd1e3b5ba2e24d84b90687f80
santipu <santipujolsau@gmail.com>
2022-11-08 13:04:23 +0100

    created state variable to store newPrice of NFT and called contract function to updateListing

--- components/UpdateListingModal.js
+++ components/UpdateListingModal.js
@@ -1,12 +1,36 @@
+import { useState } from "react"
 import { Modal, Input } from "web3uikit"
+import { useWeb3Contract } from "react-moralis"
+import nftMarketplaceAbi from "../constants/NftMarketplace.json"
+import { ethers } from "ethers"
 
-export default function UpdateListingModal({ nftAddress, tokenId, isVisible }) {
+export default function UpdateListingModal({
+    nftAddress,
+    tokenId,
+    isVisible,
+    marketplaceAddress,
+}) {
+    const [priceToUpdateListingWith, setPriceToUpdateListingWith] = useState(0)
+
+    const { runContractFunction: updateListing } = useWeb3Contract({
+        abi: nftMarketplaceAbi,
+        contractAddress: marketplaceAddress,
+        functionName: "updateListing",
+        params: {
+            nftAddress: nftAddress,
+            tokenId: tokenId,
+            newPrice: ethers.utils.parseEther(priceToUpdateListingWith || "0"),
+        },
+    })
+
     return (
         <Modal isVisible={isVisible}>
             <Input
                 label="Update listing price in L1 Currency (ETH)"
                 name="New Listing price"
                 type="number"
+                onChange={(e) => setPriceToUpdateListingWith(e.target.value)}
+                onOk={() => updateListing()}
             />
         </Modal>
     )
